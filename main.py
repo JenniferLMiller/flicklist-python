891e06eb842da81693e7f1a5c800771c26dbe487
Jennifer Miller <Jennifer@purplelime.com>
2017-01-21 08:14:55 -0600

    added 5 movies, tomorrows movie

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
-
+        movielist = ["the Big Lebowski",
+                     "Jurassic Park",
+                     "Big",
+                     "Guardians of the Galaxy",
+                     "Star Wars"]
         # TODO: randomly choose one of the movies, and return it
 
-        return "The Big Lebowski"
+        return random.choice(movielist)
 
     def get(self):
         # choose a movie by invoking our new function
@@ -20,6 +25,9 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        tomorrowMovie = self.getRandomMovie()
+        content += "<br><h1>Tomorrow's Movie</h1>"
+        content += "<p>" + tomorrowMovie + "</p>"
 
         self.response.write(content)
 
